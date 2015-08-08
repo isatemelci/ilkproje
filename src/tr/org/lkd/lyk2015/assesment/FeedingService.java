73f06deeb24d2d83b398fdd7fc30b546f9b6594e
hellfire <hellfire@hellfire.local>
2015-08-08 14:40:36 +0300

    initial commit

---
+++ src/tr/org/lkd/lyk2015/assesment/FeedingService.java
@@ -0,0 +1,13 @@
+package tr.org.lkd.lyk2015.assesment;
+
+public class FeedingService {
+
+	public void feedWithPlant(Herbivore herbivore) {
+
+	}
+
+	public void feedWithFish(Carnivore carnivore) {
+
+	}
+
+}
